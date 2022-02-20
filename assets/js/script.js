0d649389ffe7e5ef80c2bf3865422fe3d974a9bd
Michael Bee <bee.michael.02@gmail.com>
2022-02-20 12:18:31 -0600

    made column sizing better for mobile without having to make more complex responsive layout

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//make function that displays time in jumbotron id currentDAY
+
+//make function for save button to console log time and description, then save them in local storage
+
+//make function that constantly displays what's saved in local storage in each description box
+
+//make function that sets background color in description area for past, present, future
