15c54fe670d1cdff6bddffa9690faec749fbb0cc
Michael Bee <bee.michael.02@gmail.com>
2022-02-21 17:04:49 -0600

    Worked on color coding timeblocks by hour using premade past, present, and future CSS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,50 @@
 var currentDate = document.getElementById("currentDay");
 var todaysDate = moment().format("MMMM Do, YYYY");
 
+
+//function that displays date in jumbotron
 console.log(todaysDate);
 currentDate.textContent = todaysDate;
 
-//make function that displays time in jumbotron id currentDAY
 
 //make function for save button to console log time and description, then save them in local storage in an array
+    //window.localStorage.setITem(id,description)
+    //var Hour = {
+    //    id:"0900",
+    //  description: "saved text",
+    //}
+    //window.localStorage.setItem('hour', JSON.stringify(Hour));
 
 //make function that constantly displays what's saved in local storage in each description box
     // hourDiv.description = textContent.getLocalStorage.idHour
     // repeat this for all of the hourDivs
 
-
 //make function that sets background color in description area for past, present, future
     // if currentTime === currentHour {
-    //     set background to yellow highlight
+    //     set hourDiv background to yellow highlight
     // } else if currentTime > currentHour{
-    //     set  background to green highlight
+    //     set hourDiv background to green highlight
     // } else {
-    //     set background to gray
+    //     set hour Div background to lightgray
     // }
+
+    var hourNow = moment().hour();
+    console.log(hourNow);
+
+    // (array of timeblocks).each(function){
+    //     var hourBlock = pull hour id from array
+        
+        // if hourBlock < hourNow {
+        //    $(this).addClass("past");
+        //    $(this).removeClass("present");
+        //    $(this).removeClass("future");
+        // } else if hourBlock === hourNow {
+        //    $(this).removeClass("past");
+        //    $(this).addClass("present");
+        //    $(this).removeClass("future");
+        // } else {
+        //    $(this).removeClass("past");
+        //    $(this).removeClass("present");
+        //    $(this).addClass("future");
+        // }
+    //}
