2fe804158d88c2652d1232141b9a59c4c8701b5b
Michael Bee <bee.michael.02@gmail.com>
2022-02-21 08:49:36 -0600

    updated some styleing, new pseudo code, made library file for MomentJS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
+var currentDate = document.getElementById.currentDay
+
+currentDate = "Today's date"
+
 //make function that displays time in jumbotron id currentDAY
 
-//make function for save button to console log time and description, then save them in local storage
+//make function for save button to console log time and description, then save them in local storage in an array
 
 //make function that constantly displays what's saved in local storage in each description box
+    // hourDiv.description = textContent.getLocalStorage.idHour
+    // repeat this for all of the hourDivs
+
 
 //make function that sets background color in description area for past, present, future
+    // if currentTime === currentHour {
+    //     set background to yellow highlight
+    // } else if currentTime > currentHour{
+    //     set  background to green highlight
+    // } else {
+    //     set background to gray
+    // }
