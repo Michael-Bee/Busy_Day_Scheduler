4da72e82e0a3466f95b2406d3cb6208bd295f4e6
Michael Bee <bee.michael.02@gmail.com>
2022-02-22 08:09:39 -0600

    worked on scripts some more, cleaned up pseudo-code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,52 @@
+//function that displays date in jumbotron
 var currentDate = document.getElementById("currentDay");
 var todaysDate = moment().format("MMMM Do, YYYY");
-
-
-//function that displays date in jumbotron
 console.log(todaysDate);
 currentDate.textContent = todaysDate;
 
 
-//make function for save button to console log time and description, then save them in local storage in an array
-    //window.localStorage.setITem(id,description)
-    //var Hour = {
-    //    id:"0900",
-    //  description: "saved text",
-    //}
-    //window.localStorage.setItem('hour', JSON.stringify(Hour));
+//function for save button to console log time and description, then save them in local storage in an array
+    
 
-//make function that constantly displays what's saved in local storage in each description box
-    // hourDiv.description = textContent.getLocalStorage.idHour
-    // repeat this for all of the hourDivs
+window.localStorage.setITem(id,description)
+    var Hour = {
+        id:"09",
+        description: "saved text",
+        }
 
-//make function that sets background color in description area for past, present, future
-    // if currentTime === currentHour {
-    //     set hourDiv background to yellow highlight
-    // } else if currentTime > currentHour{
-    //     set hourDiv background to green highlight
-    // } else {
-    //     set hour Div background to lightgray
-    // }
+    window.localStorage.setItem('hour', JSON.stringify(Hour));
+
+//function that constantly displays what's saved in local storage in each description box
+    
+    08.description = textContent.getLocalStorage.idHour
+    09.description = textContent.getLocalStorage.idHour
+    10.description = textContent.getLocalStorage.idHour
+    11.description = textContent.getLocalStorage.idHour
+    12.description = textContent.getLocalStorage.idHour
+    13.description = textContent.getLocalStorage.idHour
+    14.description = textContent.getLocalStorage.idHour
+    15.description = textContent.getLocalStorage.idHour
+    16.description = textContent.getLocalStorage.idHour
+    17.description = textContent.getLocalStorage.idHour
+
+
+
+
+    //make function that sets background color in timeBlock
 
     var hourNow = moment().hour();
     console.log(hourNow);
 
+
     // (array of timeblocks).each(function){
-    //     var hourBlock = pull hour id from array
+    //     var timeBlock = pull hour id from array
+        //  console.log(timeBlock, hourNow)
         
-        // if hourBlock < hourNow {
+        // if timeBlock < hourNow {
         //    $(this).addClass("past");
         //    $(this).removeClass("present");
         //    $(this).removeClass("future");
-        // } else if hourBlock === hourNow {
+        // } else if timeBlock === hourNow {
         //    $(this).removeClass("past");
         //    $(this).addClass("present");
         //    $(this).removeClass("future");
