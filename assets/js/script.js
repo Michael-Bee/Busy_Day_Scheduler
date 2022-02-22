dcb518315569793716c2af3883b7296d2bc4aba2
Michael Bee <bee.michael.02@gmail.com>
2022-02-22 10:06:41 -0600

    Progress on row color function, but still not working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,54 +4,59 @@ var todaysDate = moment().format("MMMM Do, YYYY");
 currentDate.textContent = todaysDate;
 
 
-//function for save button to console log time and description, then save them in local storage in an array
-    
 
-// window.localStorage.setITem(id,description)
+//function for save button to console log time and description, then save them in local storage in an array
+
+// window.localStorage.setItem(id,description)
 //     var Hour = {
 //         id:"09",
 //         description: "saved text",
 //         }
 
-//     window.localStorage.setItem('hour', JSON.stringify(Hour));
+// window.localStorage.setItem('hour', JSON.stringify(Hour));
+
+
 
 //function that constantly displays what's saved in local storage in each description box
 //could possibly do this with for loop to avoid wet code
     
-    // $("#08 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#09 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#10 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#11 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#12 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#13 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#14 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#15 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#16 .description").val(LocalStorage.getItem(Hour, id));
-    // $("#17 .description").val(LocalStorage.getItem(Hour, id));
+    // $("#08 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#09 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#10 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#11 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#12 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#13 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#14 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#15 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#16 .description").val(LocalStorage.getItem(Hour, id, description));
+    // $("#17 .description").val(LocalStorage.getItem(Hour, id, description));
+
+//var hourDescription = document.getElementById('description);
+//hourDescription.textContent = Hour.description
 
 
-//function that sets background color in timeBlock
-// function rowColor() {
-//     var hourNow = moment().hour();
-//     console.log(hourNow);
 
-//     (Hour).each(function()) {
-//         var timeBlock = Hour.id
-//         console.log(timeBlock, hourNow)
+// function that sets background color in timeBlock
+function rowColor() {
+    var hourNow = moment().hour();
+    console.log(hourNow);
+
+    var timeBlock = this.id
+    console.log(timeBlock)
         
-//         if timeBlock < hourNow {
-//             $(this).addClass("past");
-//             $(this).removeClass("present");
-//             $(this).removeClass("future");
-//         } else if timeBlock === hourNow {
-//             $(this).removeClass("past");
-//             $(this).addClass("present");
-//             $(this).removeClass("future");
-//         } else {
-//             $(this).removeClass("past");
-//             $(this).removeClass("present");
-//             $(this).addClass("future");
-//         }
-//     }
-// }
-// rowColor();
+    if (timeBlock < hourNow) {
+        $this.addClass("past");
+        $this.removeClass("present");
+        $this.removeClass("future");
+    } else if (timeBlock === hourNow) {
+        $this.removeClass("past");
+        $this.addClass("present");
+        $this.removeClass("future");
+    } else {
+        $this.removeClass("past");
+        $this.removeClass("present");
+        $this.addClass("future");
+    }
+}
+
+rowColor();
